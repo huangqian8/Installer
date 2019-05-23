2301fab95b47edffbe585ef888be23c99a47f54b
Huangqian <bluefire1982@gmail.com>
2019-05-23 09:53:54 +0800

    Update install-python-on-raspbian.sh

--- install-python-on-raspbian.sh
+++ install-python-on-raspbian.sh
@@ -7,12 +7,13 @@ deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ stretch main non-free
 deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ stretch main non-free contrib
 EOF
 
+# Update source of Raspbian
 sudo cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
 sudo bash -c "cat > /etc/apt/sources.list.d/raspi.list" <<EOF
 deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ stretch main
 EOF
 
-
+# Install Python
 sudo apt-get update -y
 sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
 wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
@@ -27,4 +28,4 @@ rm Python-3.7.3.tar.xz
 sudo apt-get --purge remove build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
 sudo apt-get autoremove -y
 sudo apt-get clean
-python3.7
+python3.7 -V
