18083321f93e9e605cf8a3e3cdafd29dce6452a0
Huangqian <bluefire1982@gmail.com>
2019-05-23 09:31:00 +0800

    Add files via upload

---
+++ install-python-on-raspbian.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update source of Raspbian
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo bash -c "cat > /etc/apt/sources.list" <<EOF
+deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ stretch main non-free contrib
+deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ stretch main non-free contrib
+EOF
+
+sudo cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
+sudo bash -c "cat > /etc/apt/sources.list.d/raspi.list" <<EOF
+deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ stretch main
+EOF
+
+
+sudo apt-get update -y
+sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
+tar xf Python-3.7.3.tar.xz
+cd Python-3.7.3
+./configure
+make -j 4
+sudo make altinstall
+cd ..
+sudo rm -r Python-3.7.3
+rm Python-3.7.3.tar.xz
+sudo apt-get --purge remove build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
+sudo apt-get autoremove -y
+sudo apt-get clean
+python3.7
