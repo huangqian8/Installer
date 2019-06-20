f81c73c9e8e1e5adb42812f6c238677614935935
Huangqian <bluefire1982@gmail.com>
2019-06-20 13:52:50 +0800

    Update install-python-on-raspbian.sh

--- install-python-on-raspbian.sh
+++ install-python-on-raspbian.sh
@@ -23,7 +23,7 @@ cd Python-3.7.3
 make -j 4
 sudo make altinstall
 cd ..
-sudo rm -r Python-3.7.3
+sudo rm -rf Python-3.7.3
 rm Python-3.7.3.tar.xz
 sudo apt-get --purge remove build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
 sudo apt-get autoremove -y
